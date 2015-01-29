2045cc05ff4d8a1a227c4506446bb4833d2b0f84
Alex Ling <alex@home2o.com>
2015-01-28 22:10:36 -0800

    Lab 4, deployed.

--- lab2/js/clock.js
+++ lab2/js/clock.js
@@ -5,4 +5,36 @@ function getTime() {
    setTimeout(function() {getTime()}, 1000)
 }
 
+function success(data, success, xhr) {
+   console.log(data)
+   console.log(data.daily.summary)
+   $("#forecastLabel").html("<h3>" + data.daily.summary + "</h3>")
+   $("#forecastIcon").attr("src", "img/" + data.daily.icon + ".png")
+   tempMax = data.daily.data[0].temperatureMax
+   console.log(data.daily.data[0].temperatureMax)
+   
+
+   if (tempMax >= 90) {
+      $("body").attr("class", "hot")
+   }
+   else if (tempMax >= 80) {
+      $("body").attr("class", "warm")
+   }
+   else if (tempMax >= 70) {
+      $("body").attr("class", "nice")
+   }
+   else if (tempMax >= 60) {
+      $("body").attr("class", "chilly")
+   }
+   else {
+      $("body").attr("class", "cold")
+   }
+}
+
+function getTemp() {
+	var url = "https://api.forecast.io/forecast/798e623498aadf23aa27de6a253e716d/35.300399,-120.662362?callback=?";
+   $.getJSON(url, success);
+}
+
 getTime()
+getTemp()
